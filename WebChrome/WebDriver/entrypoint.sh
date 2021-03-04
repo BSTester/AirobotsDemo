a46c2094c1ef693a1ce91e93faf753e768ae6d4e
pengpeng <pengpeng@pin-dao.cn>
2021-03-04 14:03:07 +0800

    更新依赖包

--- WebChrome/WebDriver/entrypoint.sh
+++ WebChrome/WebDriver/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Java Environment Path
-export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
 export JRE_HOME=${JAVA_HOME}/jre
 export CLASSPATH=.:${JAVA_HOME}/lib:${JRE_HOME}/lib
 export PATH=${JAVA_HOME}/bin:$PATH
